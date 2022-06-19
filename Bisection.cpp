c0ae85d855d261a2893658af9b37d56a2bb12bb1
AnS-glitch <77615751+AnS-glitch@users.noreply.github.com>
2022-06-19 21:31:23 +0600

    Update and rename wave.cpp to Bisection.cpp

--- Bisection.cpp
+++ Bisection.cpp
@@ -1,10 +1,11 @@
+//Finding solutions using bisection
 #include <iostream>
 #include <cmath>
 #define EPSILON 0.000001
 using namespace std;
 
 double func (double x) {
-return ((x* exp(x))-1);
+return ((x* exp(x))-1);			//returning that function
 }
 
 void bisection (double a, double b)	{
@@ -13,11 +14,11 @@ double c;
 if ( func(a) * func(b) > 0  )	{
 	cout << "No roots" <<endl;
 }
-	while ( abs(b-a) >= EPSILON)		{
+	while ( abs(b-a) >= EPSILON)		{	//while will continue until its solution hits epsilon
 
 		c= (a+b)/2;
 if (func(c)==0)	{
-cout << c;
+cout << c;				//conditions for exact solution 
 }
 	else if (func(c) * func(a) <0)	{
 	b=c;
@@ -31,7 +32,7 @@ cout << "Root "<< c <<endl;
 
 int main ()
 {
-bisection (-10, 10);
+bisection (-10, 10);		//Might not work if multiple solutions exists
 return 0;
 }
 
