b08fb788c675bf91d06c20bc48d61d6b5642d52d
AnS-glitch <77615751+AnS-glitch@users.noreply.github.com>
2022-06-19 21:51:06 +0600

    Update and rename qoandam.cpp to Quantum.cpp

--- Quantum.cpp
+++ Quantum.cpp
@@ -1,10 +1,12 @@
+//The even wave functions for a finite square well give rise to tan(z) = sqrt[{(z_0/z)^2}-1]
+//Where, z_0 = (a/ℏ)sqrt(2mv_0) is a measure of size of the well & z= la with l = sqrt{2m(E+V_0)}/ℏ
 #include<iostream>
 #include<cmath>
 #include<fstream>
 using namespace std;
 #define a .0001
-#define h .000000006
-#define m .000009
+#define h .000000006	//Some arbitrary value since ℏ is really small
+#define m .000009	//again some abitrary value for mass of electron
 
 double  func2( double z)	{
 return (sqrt((pow((1.05/z),2))-1));
